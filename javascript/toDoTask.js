54d514ab1558ec6a7bb01609e0355aa33426c31b
Keren Samama <ksamama@g.jct.ac.il>
2024-04-19 18:32:15 +0300

    keren12

--- javascript/toDoTask.js
+++ javascript/toDoTask.js
@@ -4,8 +4,6 @@ window.onload = function() {
     var pass_value = document.querySelector('#pass input');
     
     function addList(titleIndex) {
-        
-       
         var titleSelector = '.table:nth-child(' + titleIndex + ') h1';
         // Clone the extern list
         var listTemplate = document.getElementById('list_template').content.cloneNode(true);
@@ -21,8 +19,8 @@ window.onload = function() {
     function addTask(list){
          // Clone the intern list
         var taskTemplate = document.getElementById('toDoTask').content.cloneNode(true);
-        var table_name=list.querySelector('h1').id;
-        var textBox = taskTemplate.querySelector('.myTask');
+        var table_name=list.querySelector('h1').id;//"today"
+        var textBox = taskTemplate.querySelector('.myTask');//"מה שאני נכנסת"
 
         textBox.addEventListener('dblclick', function() {
            textBox.removeAttribute('readonly');
@@ -34,19 +32,16 @@ window.onload = function() {
             var userInput = textBox.value;
             console.log("Contenu de .myTask :", userInput);
            
-            var user_check ={
+            var add_check_list ={
                 uname: uname_value.value,
-                listname:table_name,
+                listName:table_name,
                 text:userInput
             };
-            
-
- 
-            console.log(user_check.toDoLists);
-            var user_check_json=JSON.stringify(user_check);
+            console.log(add_check_list.toDoLists);
+            var add_check_list_json=JSON.stringify(add_check_list);
             var fxhttp=new FXMLHttpRequest();
             fxhttp.open("POST","./addTask", true);
-            fxhttp.send(user_check_json);
+            fxhttp.send(add_check_list_json);
             
         }
        });
@@ -61,6 +56,39 @@ window.onload = function() {
     }
 
     //delete
+
+    //UPDATE
+    function updateTask(list){
+        var taskTemplate = document.getElementById('toDoTask').content.cloneNode(true);
+        var table_name=list.querySelector('h1').id;//"today"
+        var textBox = taskTemplate.querySelector('.myTask');//"מה שאני נכנסת"
+
+        textBox.addEventListener('dblclick', function() {
+            textBox.removeAttribute('readonly');
+            const taskIndex = uname_value.toDoLists[listName].findIndex(task => task.id === taskId);
+            var updateData = {
+                uname: uname_value,
+                listName: table_name,
+                taskNum: taskIndex,
+                text:userInput
+            };
+            if (taskIndex === -1) {
+                console.error("Task not found");
+                return false;
+            }
+
+            var update_check_list_json=JSON.stringify(update_check_list);
+            var fxhttp=new FXMLHttpRequest();
+            fxhttp.open("POST","./addTask", true);
+            fxhttp.send(update_check_list_json);
+            
+            
+
+        });
+
+
+
+    }
     
 
 
